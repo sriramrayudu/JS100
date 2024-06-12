fa77fee6539c96e8840973987eed375608503d9b
sriramrayudu <109577033+sriramrayudu@users.noreply.github.com>
2024-06-12 12:58:35 +0000

    strings and nums

---
+++ 01_basics/06_numsAndMath.js
@@ -0,0 +1,10 @@
+const hundreds=10000000000
+console.log(hundreds.toLocaleString())
+//==================MATHS=====================
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.min(1,3,4,6,7,8,4,))
+const min =10
+const max =30
+//for range between 10 and 30 here we use max- min
+console.log(Math.floor(Math.random()*(max- min +1)+min))
