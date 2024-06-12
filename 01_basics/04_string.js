f77162710b20440b0c47932c7ed48272d64843ce
sriramrayudu <109577033+sriramrayudu@users.noreply.github.com>
2024-06-12 13:37:40 +0000

    dateinjs

---
+++ 01_basics/04_string.js
@@ -0,0 +1,10 @@
+const name="sriram "
+const repoCount =40
+console.log(`hello my name is ${name.toUpperCase()} and my repoCount is ${repoCount}`);
+//charAt()
+//indexOf()
+//toLowercase()
+//slice()
+//trim() trims out white spaces starting and ending places 
+const url ="https://sriram.com/sriram%20rayudu"
+console.log(url.replace('%20','-'))
